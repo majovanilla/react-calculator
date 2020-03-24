aa3da263320f921659ef7bf42afe737a1092a2e7
majovainilla <maria_reyes@me.com>
2020-03-23 23:25:06 -0600

    Edit linters

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import Big from 'big.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calculator Milestone 1</h1>
+    </div>
+  );
+}
+
+export default App;
