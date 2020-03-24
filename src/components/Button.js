0419215e770f63e415eb4d66502a44859422fdb7
majovainilla <maria_reyes@me.com>
2020-03-24 16:05:49 -0600

    Add classes to buttons using props and default props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ buttonName }) => (
-  <div className="button">
+const Button = ({ buttonName, color, wide }) => (
+  <div className={`button ${color || 'orange '} ${wide ? 'large-button' : ''}`}>
     <p className="button-text">{buttonName}</p>
   </div>
 );
 
-Button.propTypes = { buttonName: PropTypes.string.isRequired };
+Button.propTypes = {
+  buttonName: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
+};
 
 export default Button;
