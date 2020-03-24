57c30c10286e442b83b86659b202c6e0511fff2b
majovainilla <maria_reyes@me.com>
2020-03-24 15:25:11 -0600

    Edit stickler errors

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -6,37 +6,37 @@ import Button from './Button';
 const ButtonPanel = () => (
   <div className="button-panel">
     <div className="group group1">
-      <Button className="button" buttonName={'AC'} />
-      <Button className="button" buttonName={'+/-'} />
-      <Button className="button" buttonName={'%'} />
-      <Button className="button" buttonName={'+'} />
+      <Button className="button" buttonName="AC" />
+      <Button className="button" buttonName="+/-" />
+      <Button className="button" buttonName="%" />
+      <Button className="button" buttonName="+" />
     </div>
 
     <div className="group group2">
-      <Button className="button" buttonName={'7'} />
-      <Button className="button" buttonName={'8'} />
-      <Button className="button" buttonName={'9'} />
-      <Button className="button" buttonName={'X'} />
+      <Button className="button" buttonName="7" />
+      <Button className="button" buttonName="8" />
+      <Button className="button" buttonName="9" />
+      <Button className="button" buttonName="X" />
     </div>
 
     <div className="group group3">
-      <Button className="button" buttonName={'4'} />
-      <Button className="button" buttonName={'5'} />
-      <Button className="button" buttonName={'6'} />
-      <Button className="button" buttonName={'-'} />
+      <Button className="button" buttonName="4" />
+      <Button className="button" buttonName="5" />
+      <Button className="button" buttonName="6" />
+      <Button className="button" buttonName="-" />
     </div>
 
     <div className="group group4">
-      <Button className="button" buttonName={'1'} />
-      <Button className="button" buttonName={'2'} />
-      <Button className="button" buttonName={'3'} />
-      <Button className="button" buttonName={'+'} />
+      <Button className="button" buttonName="1" />
+      <Button className="button" buttonName="2" />
+      <Button className="button" buttonName="3" />
+      <Button className="button" buttonName="+" />
     </div>
 
     <div className="group group5">
-      <Button className="button" buttonName={'0'} />
-      <Button className="button" buttonName={'.'} />
-      <Button className="button" buttonName={'='} />
+      <Button className="button" buttonName="0" />
+      <Button className="button" buttonName="." />
+      <Button className="button" buttonName="=" />
     </div>
   </div>
 );
