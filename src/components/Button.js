31d6dd8682dcdd19da15ff123733102ff7634234
majovainilla <maria_reyes@me.com>
2020-03-26 11:00:33 -0600

    Add handle click to button component and to buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { render } from '@testing-library/react';
 
-const Button = ({ buttonName, color, wide }) => (
-  <div className={`button ${color} ${wide ? 'large-button' : ''}`}>
-    <p className="button-text">{buttonName}</p>
-  </div>
-);
+class Button extends React.Component {
+  handleClick = (buttonName) => {
+    const { clickHandler } = this.props;
+    return clickHandler(buttonName);
+  }
+
+  render() {
+    const { buttonName, color, wide } = this.props;
+    return (
+      <div className={`button ${color} ${wide ? 'large-button' : ''}`} onClick={() => this.handleClick(buttonName)}>
+        <p className="button-text">{buttonName}</p>
+      </div>
+    )
+  };
+}
 
 Button.propTypes = {
   buttonName: PropTypes.string.isRequired,
