b4812767437d895c3413fde77f9679655f1d9094
majovainilla <maria_reyes@me.com>
2020-03-28 17:39:06 -0600

    Change ButtonPanel component to render using map function

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,42 +4,35 @@ import Button from './Button';
 
 const ButtonPanel = ({ clickHandler }) => {
   const handleClick = buttonName => clickHandler(buttonName);
+  const buttonNames = [
+    ['AC', '+/-', '%', '÷'],
+    ['7', '8', '9', 'x'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['0', '.', '='],
+  ];
+
+  const createID = prefix => `${prefix}-${new Date().getTime()}`;
 
   return (
     <div className="button-panel">
-      <div className="group group1">
-        <Button clickHandler={handleClick} color="gray" buttonName="AC" />
-        <Button clickHandler={handleClick} color="gray" buttonName="+/-" />
-        <Button clickHandler={handleClick} color="gray" buttonName="%" />
-        <Button clickHandler={handleClick} buttonName="÷" />
-      </div>
-
-      <div className="group group2">
-        <Button clickHandler={handleClick} color="gray" buttonName="7" />
-        <Button clickHandler={handleClick} color="gray" buttonName="8" />
-        <Button clickHandler={handleClick} color="gray" buttonName="9" />
-        <Button clickHandler={handleClick} buttonName="x" />
-      </div>
-
-      <div className="group group3">
-        <Button clickHandler={handleClick} color="gray" buttonName="4" />
-        <Button clickHandler={handleClick} color="gray" buttonName="5" />
-        <Button clickHandler={handleClick} color="gray" buttonName="6" />
-        <Button clickHandler={handleClick} buttonName="-" />
-      </div>
-
-      <div className="group group4">
-        <Button clickHandler={handleClick} color="gray" buttonName="1" />
-        <Button clickHandler={handleClick} color="gray" buttonName="2" />
-        <Button clickHandler={handleClick} color="gray" buttonName="3" />
-        <Button clickHandler={handleClick} buttonName="+" />
-      </div>
-
-      <div className="group group5">
-        <Button clickHandler={handleClick} wide color="gray" buttonName="0" />
-        <Button clickHandler={handleClick} color="gray" buttonName="." />
-        <Button clickHandler={handleClick} buttonName="=" />
-      </div>
+      {
+        buttonNames.map((buttonRow, rowIndex) => (
+          <div key={createID(rowIndex)} className={`group group${rowIndex}`}>
+            {
+              buttonRow.map((buttonName, btnIndex) => (
+                <Button
+                  key={createID(btnIndex)}
+                  clickHandler={handleClick}
+                  buttonName={buttonName}
+                  color={(btnIndex === 3 || buttonName === '=') ? 'gray' : undefined}
+                  wide={buttonName === '0'}
+                />
+              ))
+            }
+          </div>
+        ))
+      }
     </div>
   );
 };
